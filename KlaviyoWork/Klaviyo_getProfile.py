e6af41a0e088479d066805e28ce104bdadefe6f3
Mitch Quarfot <mquarfot@gmail.com>
2024-03-12 12:32:43 -0600

    fix getProfile script

--- KlaviyoWork/Klaviyo_getProfile.py
+++ KlaviyoWork/Klaviyo_getProfile.py
@@ -9,7 +9,7 @@ url = "https://a.klaviyo.com/api/profiles/01HQVAH8JCMQ83VPG9PV80FTDM/"
 headers = {
     "accept": "application/json",
     "revision": "2024-02-15",
-    "Authorization": "Klaviyo-API-Key klaviyo_private_api_key"
+    "Authorization": f"Klaviyo-API-Key {klaviyo_private_api_key}"
 }
 
 response = requests.get(url, headers=headers)
